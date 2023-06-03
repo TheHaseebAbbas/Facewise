b73f24ffd2a02de14ca9fa51b8a8305456e43a0c
Haseeb Abbas <thehaseebabbas@gmail.com>
2023-06-03 16:10:43 +0500

    main screen implementation and fixes

--- app/src/main/java/com/kuro/facewise/presentation/main/MainFragment.kt
+++ app/src/main/java/com/kuro/facewise/presentation/main/MainFragment.kt
@@ -11,7 +11,6 @@ import android.view.View
 import android.view.ViewGroup
 import android.view.animation.Animation
 import android.view.animation.Transformation
-import android.widget.Toast
 import androidx.activity.result.ActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.content.FileProvider
